f40f51f65a104d9265e1c011973eb374b6b215b2
GinaCastromonte <123599985+GinaCastromonte@users.noreply.github.com>
2024-03-14 16:19:21 -0400

    bug fixes

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -8,18 +8,19 @@ const fromEmail = process.env.FROM_EMAIL;
 export async function POST(req, res) {
   const body = await req.json();
 
-  const { email, subject, message, phone } = body;
+  const { email, subject, message,phone } = body;
 
   try {
     const data = await resend.emails.send({
-      from: "gcastromonte@me.com",
-      to: "runaboutfarm37@gmail.com",
+      from: "Resend <onboarding@resend.dev>",
+      to: "runaboutfarm@hotmail.com",
       subject: subject,
       react: (
         <>
           <h3>Lori, {email} is trying to contact you</h3>
           <p>Phone:{phone}</p>
           <p>{message}</p>
+
         </>
       ),
     });
