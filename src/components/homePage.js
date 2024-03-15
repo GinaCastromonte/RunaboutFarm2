648775603eab54738c9e276f4d4b7bfd6959cf0e
GinaCastromonte <123599985+GinaCastromonte@users.noreply.github.com>
2024-03-15 15:07:32 -0400

    changed animation

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -120,7 +120,7 @@ const HomePage = () => {
         </div>
       </section>
 
-      <section data-aos="zoom-in" id="services" className="services ">
+      <section  id="services" className="services ">
         <div className="bg-services bg-cover bg-no-repeat max-w-[1466px] mx-4 xl:mx-auto rounded-[20px] xl:pt-[70px] px-6 xl:px-0 relative py-6 flex items-center xl:items-start -z-10">
           <div className="container mx-auto">
             <div className="services__top flex items-center flex-col xl:flex-row xl:mb-[60px]">
